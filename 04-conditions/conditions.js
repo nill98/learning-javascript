5f1dc6400ce092d82a1922461641db9225eef96b
Maruf <agmaruf98@gmail.com>
2025-05-29 23:51:50 +0100

    played with if,else & Switch

---
+++ 04-conditions/conditions.js
@@ -0,0 +1,46 @@
+let age = 16;
+
+if (age >= 18) {
+  console.log("You are an adult.");
+} else if (age >= 13) {
+  console.log("You are a teenager.");
+} else {
+  console.log("You are a child.");
+}
+
+
+let day = 3;
+let dayName;
+
+switch (day) {
+  case 1:
+    dayName = "Monday";
+    break;
+  case 2:
+    dayName = "Tuesday";
+    break;
+  case 3:
+    dayName = "Wednesday";
+    break;
+  case 4:
+    dayName = "Thursday";
+    break;
+  case 5:
+    dayName = "Friday";
+    break;
+  default:
+    dayName = "Weekend";
+}
+
+console.log(dayName); // Output: Wednesday
+
+
+let role = "admin";
+
+if (role === "admin") {
+  console.log("Admin panel");
+} else if (role === "user") {
+  console.log("User dashboard");
+} else {
+  console.log("Guest access");
+}
