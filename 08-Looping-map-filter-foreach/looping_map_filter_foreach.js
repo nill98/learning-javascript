04bc170019c10ab7d3abe820ae438417d47b04cc
Maruf <agmaruf98@gmail.com>
2025-06-11 21:07:42 +0100

    compleated map,foreach,filter method

--- 08-Looping-map-filter-foreach/looping_map_filter_foreach.js
+++ 08-Looping-map-filter-foreach/looping_map_filter_foreach.js
@@ -54,3 +54,28 @@ console.log(tax_added_money);
 
 
 //-------------------------------------//
+
+let numbers = [1, 2, 3, 4, 5, 6];
+
+let evens = numbers.filter(function(num) {
+  return num % 2 === 0;
+});
+
+console.log(evens); // [2, 4, 6]
+console.log("check");
+
+
+// Try to write a filter that: Keeps only scores greater than or equal to 60
+
+let scores = [50, 85, 92, 45, 77];
+// let answer = scores.filter((x) => {
+//     if (x>60) return x;
+// });
+let answer = scores.filter(x => x > 60);
+
+console.log (answer);
+
+let names = ["Alice", "Bob", "Eve", "John"];
+
+let longNames = names.filter(name => name.length > 3);
+console.log(longNames); // ["Alice", "John"]
