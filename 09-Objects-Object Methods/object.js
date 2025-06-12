7689670596c53f9de049a3f7d7d74e2569381212
Maruf <agmaruf98@gmail.com>
2025-06-12 11:28:52 +0100

    Played with Object and accessed object properties in different ways

--- 09-Objects-Object Methods/object.js
+++ 09-Objects-Object Methods/object.js
@@ -26,3 +26,34 @@ delete person.isStudent;
 for (let key in person) {
   console.log(key + ": " + person[key]);
 }
+
+
+
+let user = {
+  name: "Maruf",
+  hobbies: ["reading", "gaming"],
+  greet: function(){
+    return "Hi, Thank you for watching my codes";
+  },
+  address:{
+    city: "Sylhet",
+    zipcode: 3100,
+  }
+};
+
+console.log("User name: " +user.name);
+console.log("User hobbis: " + user.hobbies);
+console.log("User greet: " + user.greet());
+console.log("User address: " + user.address);
+console.log("User address: " + JSON.stringify(user.address));
+console.log("User city: " + user.address.city)
+console.log("User zipcode: " + user.address.zipcode);
+
+// accessing the object properties through Loop
+
+console.log("accessing through loop\n \n")
+for (let i in user) {
+  console.log(`${i}: ${user[i]}`);
+}
+
+console.log(Object.keys(user));
