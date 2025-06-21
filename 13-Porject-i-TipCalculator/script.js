c282e329b80f87cd637cf8030e5db4dbe16ffbf1
Maruf <agmaruf98@gmail.com>
2025-06-21 17:49:53 +0100

    format changed

--- 13-Porject-i-TipCalculator/script.js
+++ 13-Porject-i-TipCalculator/script.js
@@ -1,18 +1,19 @@
 // console.log("check if the connection is working")
 
 function calculateTip() {
-      const bill = parseFloat(document.getElementById("bill").value);
-      const tipPercent = parseFloat(document.getElementById("tipPercent").value);
-    
-      // Validation Check
-      if (isNaN(bill) || isNaN(tipPercent)) {
-        document.getElementById("result").innerText = "Please enter valid numbers!";
-        return;
-      }
+  const bill = parseFloat(document.getElementById("bill").value);
+  const tipPercent = parseFloat(document.getElementById("tipPercent").value);
 
-      const tip = (bill * tipPercent) / 100;
-      const total = bill + tip;
+  // Validation Check
+  if (isNaN(bill) || isNaN(tipPercent)) {
+    document.getElementById("result").innerText = "Please enter valid numbers!";
+    return;
+  }
 
-      document.getElementById("result").innerText = 
-        `Tip: $${tip.toFixed(2)} | Total: $${total.toFixed(2)}`;
-    }
+  const tip = (bill * tipPercent) / 100;
+  const total = bill + tip;
+
+  document.getElementById("result").innerText = `Tip: $${tip.toFixed(
+    2
+  )} | Total: $${total.toFixed(2)}`;
+}
