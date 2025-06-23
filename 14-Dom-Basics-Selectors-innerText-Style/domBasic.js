2a180bebf5378517bafa9fa5a0db6ab3518d16ff
Maruf <agmaruf98@gmail.com>
2025-06-23 02:27:18 +0100

    Learned the Selector

---
+++ 14-Dom-Basics-Selectors-innerText-Style/domBasic.js
@@ -0,0 +1,31 @@
+console.log("Checkin connection");
+
+// A selector is how you tell JavaScript which element(s) on your HTML page you want to work with.
+const heading = document.getElementById("title");
+
+// Selects one element by its ID (must be unique).
+console.log(heading);
+
+// Selects all elements with a specific class name (returns an HTMLCollection).
+const info = document.getElementsByClassName("info");
+console.log(info[0].textContent); // First
+
+// Selects all elements by tag (e.g., div, p, h1, etc.)
+
+const paragraphs = document.getElementsByTagName("p");
+console.log(paragraphs[1].textContent);
+
+// Selects the first matching element using CSS selector syntax.
+const para = document.querySelector(".highlight");
+console.log(para);
+
+//Selects all matching elements using CSS selector syntax (returns a NodeList).
+const allParas = document.querySelectorAll(".highlight");
+console.log(allParas[1].textContent);
+
+const title = document.getElementById("mainTitle");
+console.log(title.textContent);
+const firstInfo = document.querySelector(".info");
+console.log(firstInfo);
+const allInfos = document.querySelectorAll(".info");
+console.log(allInfos);
