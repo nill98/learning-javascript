c2d4892307bc7d06bcf49569046c8d878b6a7704
Maruf <agmaruf98@gmail.com>
2025-07-14 00:03:37 +0100

    Worked on remove and task done system

--- 17-Project-ii-TodoApp/script.js
+++ 17-Project-ii-TodoApp/script.js
@@ -16,3 +16,15 @@ function addTask() {
   }
   inputText.value = "";
 }
+
+list.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
